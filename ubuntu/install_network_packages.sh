071555a27c7bf2071ea316eca4370d6c39414474
wanix1988 <wanix1988@gmail.com>
2016-02-29 23:01:46 +0800

    1.+nmap

--- ubuntu/install_network_packages.sh
+++ ubuntu/install_network_packages.sh
@@ -6,5 +6,7 @@ sudo apt-get install -y openssh-client openssh-server openssh-sftp-server
 sudo apt-get install putty putty-tools -y
 
 #samba
-
 sudo apt-get install -y samba samba-vfs-modules
+
+#nmap
+sudo apt-get install -y nmap
